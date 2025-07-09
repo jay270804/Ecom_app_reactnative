ed4d7378503fe17f2d18cf88bfc8740a37f4ba96
jay270804 <jaypatel270804@gmail.com>
2025-07-09 17:20:48 +0530

    feat: added logo and splash screen logo

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,1 +1,2 @@
-export const ANDROID_BASE_URL = "http://192.168.89.74:8000";
+// export const ANDROID_BASE_URL = "http://192.168.116.74:8000";
+export const ANDROID_BASE_URL = "https://ecommerce-backend-uzis.onrender.com";
